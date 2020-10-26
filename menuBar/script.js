86301dbebcaca9325c86e0a7efef2da37aaebfb6
Chad Troyer <thebigticket7575@gmail.com>
2020-10-26 18:40:01 -0400

    menu hide

--- menuBar/script.js
+++ menuBar/script.js
@@ -1,12 +1,10 @@
+
+
 function show(id) {
     document.getElementById("m1").style.visibility ="hidden";
     document.getElementById("m2").style.visibility ="hidden";
     document.getElementById("m3").style.visibility ="hidden";
     document.getElementById(id).style.visibility = "visible";
-}
-
-function clearTime(id) {
-    window.clearTimeout(hide)
 }
 
 function hide(id) {
