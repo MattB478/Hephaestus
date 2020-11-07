8777c9162714f2a250e452c31c5bdf8717ac3dfb
Dre <dretaylor@iolabsllc.io>
2020-11-07 07:41:29 -0800

    backend recap

---
+++ class_code/back-end/recap.js
@@ -0,0 +1,30 @@
+// 1. The following array contains a list of grades sorted from least to greatest
+//    write a function to take in the following array and print the contents in reverse of
+//    greatest -> least
+
+arrOfGrades = [76, 77, 80, 82, 84, 87, 91, 94]
+
+
+// 2. I'm looking for a nice vacation spot for the winter and would like the warmest spot availible
+//     out of the following list. Return the city that I should visit this winter. temp is given in F
+
+cities = []
+
+city1 = {
+    name: "Moscow, Russia",
+    avgTemp: 12
+}
+
+city2 = {
+    name: "Sydney, Austrialia",
+    avgTemp: 74
+}
+
+city3 = {
+    name: "Milan, Italy",
+    avgTemp: 63
+}
+
+cities.push(city1);
+cities.push(city2);
+cities.push(city3);
